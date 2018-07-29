a1657384c0ad3e26dccaf9460c5c1241a5f01664
Kyra Crawford Calvert <kyracalvert@gmail.com>
2018-07-29 17:58:11 -0500

    Base mode functional.

--- client.js
+++ client.js
@@ -1,11 +1,49 @@
 console.log('js');
-function readyNow(){
+
+$(document).ready(readyNow);
+
+function readyNow() {
     //#submit is the button id
     $('#submit').on('click', getInput);
+    $('#employeeList').on('click', '#deleteEmployee', deleteEmployee);
+}
+
+let totalMonthly = 0;
+
+function getInput() {
+    console.log('button')
+    firstName = $('#name').val();
+    lastName = $('#surname').val();
+    id = $('#idnum').val();
+    jobTitle = $('#title').val();
+    income = $('#salary').val();
+    console.log('#getInput');
+
+    $('#employeeList').append(`
+    <tr>
+    <td>${firstName}</td>
+    <td>${lastName}</td>
+    <td>${id}</td>
+    <td>${jobTitle}</td>
+    <td>${income}</td>
+    <td> <button id = "deleteEmployee">delete</button> </td>
+</tr>
+    `)
+
+    totalMonthly = totalMonthly + (income / 12);
+    console.log('total monthly:', totalMonthly);
+    $('#expenses').html('$' + Math.round(100 * totalMonthly) / 100);
+
+    if (totalMonthly > 20000) {
+        $('#expenses').css('background-color', 'red');
+    }
+
 
 }
 
-function getInput(){
+function deleteEmployee() {
+    console.log('deleteEmployee');
+    $(this).parent().parent().remove();
 
 }
 
